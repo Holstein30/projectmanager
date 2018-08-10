0377f739195e642305cacb552c95b97bd22f1650
Dale Holstein <holstein30@live.com>
2018-08-10 15:38:37 -0400

    projects imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Projects from "./Components/Projects";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">My App</div>;
+    return (
+      <div className="App">
+        My App
+        <Projects />
+      </div>
+    );
   }
 }
 
